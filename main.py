ffb00dc0938ef543f5b5708212af8679c7716913
OleksiiPavlyk <pavlyk.oleksii@gmail.com>
2026-07-13 08:46:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 print("Hello World")
 print("Hello Git")
+
 def is_adult(age): return age >= 18
 
 # Виклик функції, який поверне True або False
@@ -8,3 +9,7 @@ print(can_vote) # Виведе: True
 
 name = "Wendy\'s dog"
 print(name)   # Wendy's dog
+
+x = 2
+y = x + 10
+print(y)  # Виведе: 12
