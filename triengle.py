219a7f77a296efce5c93f8c63d663676a4f98f28
OleksiiPavlyk <pavlyk.oleksii@gmail.com>
2026-07-14 22:09:16 +0300

    1407

---
+++ triengle.py
@@ -0,0 +1,9 @@
+side_a = 10
+side_b = 5
+
+hypotenuse = (side_a ** 2 + side_b ** 2) ** 0.5
+print("The length of the hypotenuse is:", hypotenuse)  # Вив
+
+S = side_a * side_b/2
+print("The area of the triangle is:", S, type(S))  # Виведе: 25.0
+
