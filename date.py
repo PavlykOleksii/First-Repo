8a155a33c3fc65809845b159e9b4b68788eb5533
OleksiiPavlyk <pavlyk.oleksii@gmail.com>
2026-07-21 08:52:08 +0300

    13

--- date.py
+++ date.py
@@ -40,9 +40,32 @@
 
 # print(specific_date)  # Виведе "2020-01-07 00:00:00"
 
-import datetime
+# import datetime
 
-# Створення об'єкта datetime з конкретною датою і часом
-specific_datetime = datetime.datetime(year=2020, month=1, day=7, hour=14, minute=30, second=15)
+# # Створення об'єкта datetime з конкретною датою і часом
+# specific_datetime = datetime.datetime(year=2020, month=1, day=7, hour=14, minute=30, second=15)
 
-print(specific_datetime)  # Виведе "2020-01-07 14:30:15"
+# print(specific_datetime)  # Виведе "2020-01-07 14:30:15"
+
+# from datetime import datetime
+
+# # Створення об'єкта datetime
+# now = datetime.now()
+
+# # Отримання номера дня тижня
+# day_of_week = now.weekday()
+
+# # Поверне число від 0 (понеділок) до 6 (неділя)
+# print(f"Сьогодні: {day_of_week}")  
+
+from datetime import datetime
+
+# Створення двох об'єктів datetime
+datetime1 = datetime(2023, 3, 14, 12, 0)
+datetime2 = datetime(2023, 3, 15, 12, 0)
+
+# Порівняння дат
+print(datetime1 == datetime2)  # False, тому що дати не однакові
+print(datetime1 != datetime2)  # True, тому що дати різні
+print(datetime1 < datetime2)   # True, тому що datetime1 передує datetime2
+print(datetime1 > datetime2)   # False, тому що datetime1 не наступає за datetime2
---
+++ timedelta.py
@@ -0,0 +1,21 @@
+# from datetime import timedelta
+# delta = timedelta(
+#     days=50,
+#     seconds=27,
+#     microseconds=10,
+#     milliseconds=29000,
+#     minutes=5,
+#     hours=8,
+#     weeks=2
+# )
+# print(delta)
+
+from datetime import datetime
+
+seventh_day_2019 = datetime(year=2009, month=1, day=7, hour=14)
+seventh_day_2020 = datetime(year=2020, month=1, day=7, hour=14)
+
+difference = seventh_day_2020 - seventh_day_2019
+print(difference)  # 3650 days, 0:00:00
+print(difference.total_seconds())  # 31536000.0
+difference.
