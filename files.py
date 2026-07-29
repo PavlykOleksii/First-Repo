7ea65c2af6d810d43cde6ef2051f869eaa40a07b
Oleksii Pavlyk <pavlyk.oleksii@gmail.com>
2026-07-29 09:23:23 +0300

    19

--- files.py
+++ files.py
@@ -1,3 +1,26 @@
-fh = open('test.txt')
-# операції над файлом
+strtxt = "У Python є абстракція над файлами\n — це вказівник на файл\n або файловий об'єкт.\n Нічого складного в цьому немає, \nце навпаки спрощує роботу з багатьма системними ресурсами."
+# fh = open('test.txt', 'a', encoding = "utf-8")
+# fh.write("У Python є абстракція над файлами — це вказівник на файл або файловий об'єкт. Нічого складного в цьому немає, це навпаки спрощує роботу з багатьма системними ресурсами.")
+# fh.close()
+
+# fh1 = open('test.txt')
+# print(fh1.read())
+# # операції над файлом
+# fh1.close()
+#print(strtxt)
+fh = open("test1.txt","w+",encoding="utf-8")
+symlwr = fh.write(strtxt)
+five_symbl = fh.read()
+
 fh.close()
+
+fha = open("test1.txt","r",encoding="utf-8")
+
+line = list()
+
+for lin in fha:
+    line.append(lin.strip())
+
+
+print(line)
+fha.close()
