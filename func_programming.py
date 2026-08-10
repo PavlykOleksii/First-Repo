4dc4f04c8c764f13d639500c9dd28adbc5f7161a
Oleksii Pavlyk <pavlyk.oleksii@gmail.com>
2026-08-10 08:06:39 +0300

    30

--- func_programming.py
+++ func_programming.py
@@ -12,8 +12,8 @@
 # def multiply(a: int, b: int) -> int:
 #     return a * b
 
-# def apply_operation(a: int, b: int, operation: Callable[[int, int], int]) -> int:
-#     return operation(a, b)
+# def apply_operation(a: int, b: int, oper: Callable[[int, int], int]) -> int:
+#     return oper(a, b)
 
 # # Використання
 # result_add = apply_operation(5, 3, add)
@@ -21,16 +21,150 @@
 
 # print(result_add, result_multiply)
 
-from typing import Callable
+# from typing import Callable
 
-def power(exponent: int) -> Callable[[int], int]:
-    def inner(base: int) -> int:
-        return base ** exponent
-    return inner
+# def power(exponent: int) -> Callable[[int], int]:
+#     def inner(base: int) -> int:
+#         return base ** exponent
+#     return inner
 
-# Використання
-square = power(2)
-cube = power(3)
+# # Використання
+# square = power(2)
+# cube = power(3)
 
-print(square(4)) 
-print(cube(4))
+# print(square(4)) 
+# print(cube(4))
+
+# from typing import Callable, Dict
+
+# # Визначення функцій
+# def add(a: int, b: int) -> int:
+#     return a + b
+
+# def multiply(a: int, b: int) -> int:
+#     return a * b
+
+# def power(exponent: int) -> Callable[[int], int]:
+#     def inner(base: int) -> int:
+#         return base ** exponent
+#     return inner
+
+# # Використання power для створення функцій square та cube
+# square = power(2)
+# cube = power(3)
+
+# # Словник операцій
+# operations: Dict[str, Callable] = {
+#     'add': add,
+#     'multiply': multiply,
+#     'square': square,
+#     'cube': cube
+# }
+
+# # Використання операцій
+# result_add = operations['add'](10, 20)  # 30
+# result_square = operations['square'](5)  # 25
+
+# print(result_add)  
+# print(result_square)  
+
+# from typing import Callable
+
+# def counter() -> Callable[[], int]:
+#     count = 0
+
+#     def increment() -> int:
+#         # використовуємо nonlocal, щоб змінити змінну в замиканні
+#         nonlocal count  
+#         count += 1
+#         return count
+
+#     return increment
+
+# # Створення лічильника
+# count_calls = counter()
+
+# # Виклики лічильника
+# print(count_calls())  # Виведе 1
+# print(count_calls())  # Виведе 2
+# print(count_calls())  # Виведе 3
+
+# def add(a):
+#     def add_b(b):
+#         return a + b
+#     return add_b
+
+# # Використання:
+# add_5 = add(5)
+# result = add_5(10)
+# print(result)
+
+# def apply_discount(price: float, discount_percentage: int) -> float:
+#     return price * (1 - discount_percentage / 100)
+
+# # Використання
+# discounted_price = apply_discount(500, 10)  # Знижка 10% на ціну 500
+# print(discounted_price)
+
+# discounted_price = apply_discount(500, 20)  # Знижка 20% на ціну 500
+# print(discounted_price)
+
+# from typing import Callable
+
+# def discount(discount_percentage: int) -> Callable[[float], float]:
+#     def apply_discount(price: float) -> float:
+#         return price * (1 - discount_percentage / 100)
+#     return apply_discount
+
+# # Каррінг у дії
+# ten_percent_discount = discount(10)
+# twenty_percent_discount = discount(20)
+
+# # Застосування знижок
+# discounted_price = ten_percent_discount(500)  # 450.0
+# print(discounted_price)
+
+# discounted_price = twenty_percent_discount(500)  # 400.0
+# print(discounted_price)
+
+# def complicated(x: int, y: int) -> int:
+#     return x + y
+
+# def logger(func):
+#     def inner(x: int, y: int) -> int:
+#         print(f"Викликається функція: {func.__name__}: {x}, {y}")
+#         result = func(x, y)
+#         print(f"Функція {func.__name__} завершила виконання: {result}")
+#         return result
+
+#     return inner
+
+# complicated = logger(complicated)
+# print(complicated(2, 3))
+
+# from functools import wraps
+
+# def logger(func):
+#     @wraps(func)
+#     def inner(x: int, y: int) -> int:
+#         print(f"Викликається функція: {func.__name__}: {x}, {y}")
+#         result = func(x, y)
+#         print(f"Функція {func.__name__} завершила виконання: {result}")
+#         return result
+
+#     return inner
+
+# @logger
+# def complicated(x: int, y: int) -> int:
+#     return x + y
+
+# print(complicated(2, 3))
+# print(complicated.__name__)
+
+def is_positive(x):
+    return x > 0
+
+nums = [-2, -1, 0, 1, 2]
+positive_nums = filter(is_positive, nums)
+
+print(list(positive_nums))
