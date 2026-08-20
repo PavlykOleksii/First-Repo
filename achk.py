93c4c4ab9f7280f70a2cd1ac504d7473f4f86731
Oleksii Pavlyk <pavlyk.oleksii@gmail.com>
2026-08-21 00:03:37 +0300

    33

--- achk.py
+++ achk.py
@@ -23,16 +23,68 @@ class Point:
         if (type(y) == int) or (type(y) == float):
             self.__y = y
 
+    def __str__(self):
+        return f"Point({self.x},{self.y})"
+
 
 class Vector:
     def __init__(self, coordinates: Point):
         self.coordinates = coordinates
 
     def __setitem__(self, index, value):
-        
-            
-        
-            
+        if index == 0:
+            self.coordinates.x = value
+        if index == 1:
+            self.coordinates.y = value
 
     def __getitem__(self, index):
+        if index == 0:
+            return self.coordinates.x
+        if index == 1:
+            return self.coordinates.y
+
+    def __call__(self, value=None):
+        if value:
+            self.coordinates.x = self.coordinates.x * value
+            self.coordinates.y = self.coordinates.y * value
+        return self.coordinates.x, self.coordinates.y
+
+    def __add__(self, vector):
+        x = self.coordinates.x + vector.coordinates.x
+        y = self.coordinates.y + vector.coordinates.y
+        return Vector(Point(x, y))
+
+    def __sub__(self, vector):
+        x = self.coordinates.x - vector.coordinates.x
+        y = self.coordinates.y - vector.coordinates.y
+        return Vector(Point(x, y))
+
+    def __mul__(self, vector):
+        return (
+                self.coordinates.x * vector.coordinates.x
+                + self.coordinates.y * vector.coordinates.y
+        )
+
+    def len(self):
+        return (self.coordinates.x ** 2 + self.coordinates.y ** 2) ** 0.5
+
+    def __str__(self):
+        return f"Vector({self.coordinates.x},{self.coordinates.y})"
+
+    def __eq__(self, vector):
+        
+
+    def __ne__(self, vector):
+        
+
+    def __lt__(self, vector):
+        
+
+    def __gt__(self, vector):
+        
+
+    def __le__(self, vector):
+        
+
+    def __ge__(self, vector):
         
